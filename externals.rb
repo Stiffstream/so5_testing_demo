46318dc5c5d0e34b94b42c094e6fd9ae5dbb52aa
eao197 <eao197@gmail.com>
2019-01-08 13:45:43 +0300

    Switch to the final version of SO-5.5.24.

--- externals.rb
+++ externals.rb
@@ -1,13 +1,9 @@
-MxxRu::svn_externals :so5 do |e|
-  e.url 'https://svn.code.sf.net/p/sobjectizer/repo/branches/so_5/5.5'
-  e.rev 3693
+MxxRu::arch_externals :so5 do |e|
+  e.url 'https://sourceforge.net/projects/sobjectizer/files/sobjectizer/SObjectizer%20Core%20v.5.5/so-5.5.24.zip'
+
+  e.sha512 'd33b5660fb6d7b1182d68069bb86782dd7632071723762875d88dd6d67353f6913b25e0574d87e06b86f472608631f59c3e26dc9c67cda9f8223454ae758b524'
 
   e.map_dir 'dev/so_5' => 'dev'
-end
-
-MxxRu::svn_externals :timertt do |e|
-  e.url 'https://svn.code.sf.net/p/sobjectizer/repo/tags/timertt/1.2.2'
-
   e.map_dir 'dev/timertt' => 'dev'
 end
 
