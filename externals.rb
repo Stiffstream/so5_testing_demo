c2062b79dd94a4156c902eb8cf82f0dd3680d2a5
Yauheni Akhotnikau <eao197@gmail.com>
2023-06-30 11:41:44 +0300

    Adaptation to SO-5.8.0.

--- externals.rb
+++ externals.rb
@@ -1,11 +1,11 @@
 MxxRu::arch_externals :so5 do |e|
-  e.url 'https://github.com/Stiffstream/sobjectizer/releases/download/v.5.7.2/so-5.7.2.zip'
+  e.url 'https://github.com/Stiffstream/sobjectizer/archive/v.5.8.0.tar.gz'
 
   e.map_dir 'dev/so_5' => 'dev'
 end
 
 MxxRu::arch_externals :doctest do |e|
-  e.url 'https://github.com/onqtam/doctest/archive/2.4.1.tar.gz'
+  e.url 'https://github.com/onqtam/doctest/archive/v2.4.11.tar.gz'
 
   e.map_file 'doctest/doctest.h' => 'dev/doctest/*'
 end
